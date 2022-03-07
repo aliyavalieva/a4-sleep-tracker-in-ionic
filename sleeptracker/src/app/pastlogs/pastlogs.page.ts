1884948c05a6613f79245d2861f9a1a4134c14fd
AsadEllis <ellis909@gmail.com>
2022-03-06 21:03:26 -0800

    Remove of pastlogs

--- sleeptracker/src/app/pastlogs/pastlogs.page.ts
+++ sleeptracker/src/app/pastlogs/pastlogs.page.ts
@@ -1,40 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { SleepService } from '../services/sleep.service';
-import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
-import { OvernightSleepData } from '../data/overnight-sleep-data';
 
 @Component({
-  selector: 'app-Pastlogs',
-  templateUrl: './Pastlogs.page.html',
-  styleUrls: ['./Pastlogs.page.scss'],
+  selector: 'app-pastlogs',
+  templateUrl: './pastlogs.page.html',
+  styleUrls: ['./pastlogs.page.scss'],
 })
 export class PastlogsPage implements OnInit {
-  testArray:string[] = ['one','two','three','four'];
-  overnightSleepDataArray: OvernightSleepData[];
-  sleepinessDataArray: StanfordSleepinessData[];
-  showSleepDataCards: boolean;
-  showSleepinessDataCards: boolean;
 
-  constructor(private sleepService:SleepService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.overnightSleepDataArray = SleepService.AllOvernightData;
-    this.sleepinessDataArray = SleepService.AllSleepinessData;
-    this.showSleepDataCards = true;
-    this.showSleepinessDataCards = false;
-  }
-
-  segmentChanged(ev: any) {
-    if(ev.detail.value=="sleepData") {
-      
-      console.log(this.overnightSleepDataArray);
-      
-      this.showSleepDataCards = true; 
-      this.showSleepinessDataCards = false;
-    } else if (ev.detail.value=="sleepinessData"){
-      this.showSleepDataCards = false;
-      this.showSleepinessDataCards = true;
-    }
   }
 
 }
